31a8e0d0bb9581a11c1a3c69fba6ef623695110e
hbs <hbs@HIB-11616.hvl.no>
2018-01-19 10:35:18 +0100

    v01

---
+++ Karakter/src/no/hib/dat102/FeilKarakterException.java
@@ -0,0 +1,20 @@
+package no.hib.dat102;
+
+/**
+ * Feilhåndtering for karakter.
+ * 
+ * @author Ole Olsen
+ */
+public class FeilKarakterException extends Exception {
+
+	/**
+	 * Lager et nytt unntak.
+	 * 
+	 * @param melding
+	 *            melding
+	 */
+	public FeilKarakterException(String melding) {
+		super(melding);
+	}
+
+}
