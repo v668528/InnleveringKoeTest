e91582bf4745eedbf839e112d292b2c3d9dc204e
hbs <hbs@HIB-11616.hvl.no>
2018-02-05 17:04:44 +0100

    version1

---
+++ ListeOrdnetU/src/no/hib/dat102/kjedet/KjedetOrdnetListe.java
@@ -0,0 +1,111 @@
+package no.hib.dat102.kjedet;
+import no.hib.dat102.adt.*;
+/**
+ * 
+ * @param <T>
+ *            elementtypen
+ */
+public class KjedetOrdnetListe<T extends Comparable<T>> implements OrdnetListeADT<T> {
+	private int antall;
+	private LinearNode<T> foerste, siste;
+
+	/**
+	 * Lager en ny tom liste.
+	 */
+	public KjedetOrdnetListe() {
+		antall = 0;
+		foerste = null;
+		siste = null;
+	}
+
+	@Override
+	public T fjernFoerste() {
+		T svar = null;
+		//...Fyll ut
+		return svar;
+	}
+
+	@Override
+	public T fjernSiste() {
+		T svar = null;
+		//...Fyll ut
+		return svar;
+	}
+
+	@Override
+	public T foerste() {
+		T svar = null;
+		if (!erTom()) {
+			svar = foerste.getElement();
+		}
+		return svar;
+	}
+
+	@Override
+	public T siste() {
+		T svar = null;
+		if (!erTom()) {
+			svar = siste.getElement();
+		}
+		return svar;
+	}
+
+	@Override
+	public boolean erTom() {
+		return antall == 0;
+	}
+
+	@Override
+	public int antall() {
+		return antall;
+	}
+
+	@Override
+	public void leggTil(T element) {
+
+		//...Fyll ut
+	}
+
+
+	@Override
+	public T fjern(T element) {
+		T svar = null;
+		LinearNode<T> forrige = null, denne = foerste;
+		while (denne != null && element.compareTo(denne.getElement()) > 0) {
+			forrige = denne;
+			denne = denne.getNeste();
+		}
+		if (denne != null && element.equals(denne.getElement())) { // funnet
+			antall--;
+			svar = denne.getElement();
+			if (forrige == null) {     // Første element
+				foerste = foerste.getNeste();
+				if (foerste == null) { // Tom liste
+					siste = null;
+				}
+			} else { // Inni listen eller bak
+				forrige.setNeste(denne.getNeste());
+				if (denne == siste) { // bak
+					siste = forrige;
+				}
+			}
+		} // ikke-funn
+		return svar;
+	}
+
+	@Override
+	public boolean inneholder(T element) {
+		LinearNode<T> denne = foerste;
+		boolean resultat = false;
+		while (denne != null && element.compareTo(denne.getElement()) > 0) {
+			denne = denne.getNeste();
+		}
+		if (denne != null) {
+			if (element.equals(denne.getElement())) {
+				resultat = true;
+			}
+		} // ikke-funn
+		return resultat;
+	}
+
+}// class
