e4bd1e76b824daa23333651d0f9129d73164afad
hbs <hbs@HIB-11616.hvl.no>
2018-02-09 13:34:01 +0100

    version1 til oving4

---
+++ BalanseringU/src/no/hib/dat102/Balansering.java
@@ -0,0 +1,64 @@
+package no.hib.dat102;
+
+import java.io.*;
+
+public class Balansering {
+	// Her opphever du kommentarsetning når du har fått lagt inn
+	// nødvendig kode
+	// SirkulaerStabel<Parentesinfo>stabel = new
+	// SirkulaerStabel<Parentesinfo>();
+
+	private boolean passer(char åpent, char lukket) {
+		switch (åpent) {
+		case '(':
+			return (lukket == ')');
+		case '[':
+			return (lukket == ']');
+		case '{':
+			return (lukket == '}');
+		default:
+			return false;
+		}
+	}//
+
+	public void foretaBalansering(String innDataStreng, int linjenr) {
+
+		int lengde = innDataStreng.length();
+		// Fyll ut
+
+	}//
+
+	public void lesFraFil(String filnavn) {
+		FileReader tekstFilLeser = null;
+		try {
+			tekstFilLeser = new FileReader(filnavn);
+		} catch (FileNotFoundException unntak) {
+			System.out.println("Finner ike filen!");
+			System.exit(-1);
+		}
+
+		BufferedReader tekstLeser = new BufferedReader(tekstFilLeser);
+		String linje = null;
+		int linjenr = 0;
+		try {
+			linje = tekstLeser.readLine();
+			while (linje != null) {
+				// kalle metode her!
+				// Fyll ut
+
+			} // while
+		}
+
+		catch (IOException unntak) {
+			System.out.println("Feil ved innlesing!");
+			System.exit(-1);
+		}
+		try {
+			tekstFilLeser.close();
+		} catch (IOException unntak) {
+			System.out.println("Feil ved lukking av fil!");
+		}
+
+	}// metode
+
+}// class
