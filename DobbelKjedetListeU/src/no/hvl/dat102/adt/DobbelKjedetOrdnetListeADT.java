e9cc824dfee5034f5a230995329d1c89cd3352ca
Kronstad <Harald.Soleim@hib.no>
2022-02-05 13:21:02 +0100

    v01

---
+++ DobbelKjedetListeU/src/no/hvl/dat102/adt/DobbelKjedetOrdnetListeADT.java
@@ -0,0 +1,32 @@
+package no.hvl.dat102.adt;
+
+public interface DobbelKjedetOrdnetListeADT<T> {
+
+	/**
+	 * 
+	 * @param el er elementet som skal legges til
+	 */
+	void leggTil(T el);
+
+	/**
+	 * 
+	 * @param el er elementet som skal fjernes hvis det fins
+	 * @return elementet som skal fjernes ellers returneres null
+	 * @exception EmptyCollectionException unntak kastes dersom listen allerede er
+	 *                                     tom
+	 */
+	T fjern(T el);
+
+	/**
+	 * 
+	 * @return returner true hvis listen er tom ellers false
+	 */
+	boolean erTom();
+
+	/**
+	 * 
+	 * @return returnerer antall elementer
+	 */
+	int antall();
+
+}
