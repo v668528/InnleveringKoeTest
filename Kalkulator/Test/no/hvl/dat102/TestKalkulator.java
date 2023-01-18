7ba07e365b2ed6d334fe2ed224c874a8350ecaf8
Kronstad <Harald.Soleim@hib.no>
2023-01-18 18:52:14 +0100

    v01

--- Kalkulator/Test/no/hvl/dat102/TestKalkulator.java
+++ Kalkulator/Test/no/hvl/dat102/TestKalkulator.java
@@ -1,34 +1,35 @@
 package no.hvl.dat102;
 
-import static org.junit.Assert.*;
-
-import org.junit.Test;
-
-import no.hvl.dat102.Kalkulator;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.junit.jupiter.api.Test;
 
 public class TestKalkulator {
-	//testmetoder
 
 	@Test
 	public void testAdd() {
-		assertEquals("Test av 1+2", 3, Kalkulator.add(1, 2));
+		assertEquals(3, Kalkulator.add(1, 2), "Test av 1+2");
 	}
 
 	@Test
 	public void testSub() {
-		assertEquals("Test av 4-2", 2, Kalkulator.sub(4, 2));
-		assertEquals("Test av 2-4", -2, Kalkulator.sub(2, 4));
-
+		assertEquals(2, Kalkulator.sub(4, 2), "Test av 4-2");
+		assertEquals(-2, Kalkulator.sub(2, 4), "Test av 2-4");
 	}
 
 	@Test
 	public void testMul() {
-		assertEquals("Test av 3*2", 6, Kalkulator.mul(3, 2));
+		assertEquals(6, Kalkulator.mul(3, 2), "Test av 3*2");
 	}
 
 	@Test
 	public void testDiv() {
-		assertEquals("Test av 4/2", 2, Kalkulator.div(4, 2));
+		assertEquals(2, Kalkulator.div(4, 2), "Test av 4/2");
+	}
+
+	@Test
+	public void testNullDiv() {
+		assertThrows(ArithmeticException.class, () -> Kalkulator.div(4, 0));
 	}
 
 }
--- Kalkulator/src/no/hvl/dat102/Kalkulator.java
+++ Kalkulator/src/no/hvl/dat102/Kalkulator.java
@@ -26,7 +26,6 @@ public class Kalkulator {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-
 	}
 
 }
