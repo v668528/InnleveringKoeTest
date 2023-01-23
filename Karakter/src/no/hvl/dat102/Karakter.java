3dc701e1ad0650fc8d386836269aad7580544d90
Kronstad <Harald.Soleim@hib.no>
2023-01-23 10:45:32 +0100

    v01 runtime

--- Karakter/src/no/hvl/dat102/Karakter.java
+++ Karakter/src/no/hvl/dat102/Karakter.java
@@ -7,7 +7,7 @@ public class Karakter {
 
 	//  Beregn karakter
 	// Poengsum mellom 0 and 100 (inklusive)
-	public static char beregnKarakter(int poeng) throws FeilKarakterException {
+	public static char beregnKarakter(int poeng)  {
 		char kar;
 		if (poeng < 0 || poeng > 100) {
 			throw new FeilKarakterException("ulovlig poeng " + poeng);
